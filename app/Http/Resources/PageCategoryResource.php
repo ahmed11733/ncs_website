a5bd76596f66220f98f87c3e73fd24b1afaa5d2d
Ahmed Abdelhady <ahmedabdelhady@192.168.1.6>
2025-09-20 21:30:11 +0300

    Fix pages categorries name ar issue

--- app/Http/Resources/PageCategoryResource.php
+++ app/Http/Resources/PageCategoryResource.php
@@ -9,9 +9,11 @@ class PageCategoryResource extends JsonResource
 {
     public function toArray(Request $request)
     {
+        $locale = $request->header('Accept-Language');
+
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'name' => $this->getTranslation('name', $locale),
             'pages' => PageResource::collection($this->whenLoaded('pages')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
